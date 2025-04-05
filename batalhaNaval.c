f7eae14bfda0f97b04ec6b6bf4591bd6694a50db
Reginaldo Ytalo <90566773+YtalloXD@users.noreply.github.com>
2025-04-05 04:29:32 +0000

    atualizado a documentação dentro do código

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+// Definindo as contantes para o tamanho das habilidades
+
 const int LINHAS = 3;
 const int COLUNAS = 5;
+
+// Funções para chamar durante o int main(), imprimindo assim os tabuleiros, os navios, e as habilidades no segundo tabuleiro
 
 void imprimirTabuleiro(int tabuleiro1[10][10]) {
     for (int i = 0; i < 10; i++) {
@@ -13,7 +17,7 @@ void imprimirTabuleiro(int tabuleiro1[10][10]) {
     }
 }
 
-void imprimirTabuleiroHabilidade(int tabuleiro2[10][10]) {
+void imprimirTabuleiro2(int tabuleiro2[10][10]) {
     for (int i = 0; i < 10; i++) {
         printf("%2d ", i + 1); // Print row numbers
         for (int j = 0; j < 10; j++) {
@@ -22,6 +26,8 @@ void imprimirTabuleiroHabilidade(int tabuleiro2[10][10]) {
         printf("\n");
     }
 }
+
+// Funções para inserir as habilidades no tabuleiro2, que é o tabuleiro de habilidades
 
 void habConeInput(int tabuleiro2[10][10], int habCone[3][5], int LINHAS, int COLUNAS) {
     for(int i = 0; i < 3; i++){
@@ -53,27 +59,32 @@ void habOctaedroInput(int tabuleiro2[10][10], int habOctaedro[3][5], int LINHAS,
     }
 }
 
+//Função que inicializa o projeto
+
 int main() {
 	
     int tabuleiro1[10][10] = {0};
     int tabuleiro2[10][10] = {0};
 
+    int navio1 = 1;
+    int navio2 = 2;
+    int navio3 = 3;
+    int navio4 = 4;
+
+    
     int habCone[3][5] = {{0, 0, 5, 0, 0},
                          {0, 5, 5, 5, 0},
                          {5, 5, 5, 5, 5}};
 
-    int habCruz[3][5] = {{0, 0, 5, 0, 0},
-                         {5, 5, 5, 5, 5},
-                         {0, 0, 5, 0, 0}};
+    int habCruz[3][5] = {{0, 0, 6, 0, 0},
+                         {6, 6, 6, 6, 6},
+                         {0, 0, 6, 0, 0}};
 
-    int habOctaedro[3][5] = {{0, 0, 5, 0, 0},
-                            {0, 5, 5, 5, 0},
-                            {0, 0, 5, 0, 0}};
+    int habOctaedro[3][5] = {{0, 0, 7, 0, 0},
+                             {0, 7, 7, 7, 0},
+                             {0, 0, 7, 0, 0}};
 
-    int navio1 = 3;
-    int navio2 = 3;
-    int navio3 = 3;
-    int navio4 = 3;
+//Certos pontos do tabuleiro1 são preenchidos com os navios com os seus respectivos números, e o restante é preenchido com 0s, que representam a água
 
     tabuleiro1[4][1] = navio1;
     tabuleiro1[4][2] = navio1;
@@ -91,22 +102,21 @@ int main() {
     tabuleiro1[7][4] = navio4;
     tabuleiro1[8][5] = navio4;
 
+//Output dos dois tabuleiros, navios no primeiro tabuleiro, e as habilidades no segundo tabuleiro
 
-    // tabuleiro2[1][4] = habCone[5][6];
-    // tabuleiro2[4][7] = habCruz[1][2];
-    // tabuleiro2[8][7] = habOctaedro[8][6];
-
-    printf("\n### TABULEIRO BATALHA NAVAL ###\n");
+    printf("\n### TABULEIRO BATALHA NAVAL ###\n1- Navio 1 | 2- Navio 2 | 3- Navio 3 | 4- Navio 4\n");
     printf("   A B C D E F G H I J\n");
+    
     imprimirTabuleiro(tabuleiro1);
     
+    
+    
+    printf("\n### TABULEIRO BATALHA NAVAL HABILIDADES ###\n5- Cone | 6- Cruz | 7- Octaedro\n");
+    printf("   A B C D E F G H I J\n");
+
     habConeInput(tabuleiro2, habCone, 1, 1);
     habCruzInput(tabuleiro2, habCruz, 6, 3);
     habOctaedroInput(tabuleiro2, habOctaedro, 1, 6);
-    
-
-    printf("\n### TABULEIRO BATALHA NAVAL HABILIDADES ###\n");
-    printf("   A B C D E F G H I J\n");
-    imprimirTabuleiroHabilidade(tabuleiro2);
+    imprimirTabuleiro2(tabuleiro2);
     return 0;
 }
